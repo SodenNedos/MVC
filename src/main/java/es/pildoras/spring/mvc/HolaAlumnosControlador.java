6e2c90dab5a3a2a6858bff26b68fa75f1e7e22e7
Soden <f.navarronicolas@gmail.com>
2020-04-11 19:49:57 +0200

    Sigo el curso de Spring.

--- src/main/java/es/pildoras/spring/mvc/HolaAlumnosControlador.java
+++ src/main/java/es/pildoras/spring/mvc/HolaAlumnosControlador.java
@@ -7,28 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HolaAlumnosControlador {
-	
-	//Propociona el formulario
+
+	// Propociona el formulario
 	@RequestMapping("/muestraFormulario")
 	public String muestraFormulario() {
 		return "HolaAlumnosFormulario";
 	}
-	
+
 	@RequestMapping("/procesarFormulario")
 	public String procesarFormulario() {
 		return "HolaAlumnosSpring";
 	}
-	
-	
+
 	@RequestMapping("/procesarFormulario2")
 	public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
-		String nombre=request.getParameter("nombreAlumno");
-		nombre+=" es el mejor alumno";
-		
-		String mensajefinal= "¿Quién es el mejor alumno? "+ nombre;
-		
+		String nombre = request.getParameter("nombreAlumno");
+		nombre += " es el mejor alumno";
+
+		String mensajefinal = "¿Quién es el mejor alumno? " + nombre;
+
 		modelo.addAttribute("mensajeClaro", mensajefinal);
-		
+
 		return "HolaAlumnosSpring";
 	}
 }
